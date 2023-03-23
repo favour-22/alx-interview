2266e06ab4d75d6bc2034b1631a07d258864865b
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-03-23 02:07:09 +0100

    fix typos

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
     """
-    Returns a string representation of Pascal's Triangle up to n rows.
+    Returns a list of lists of integers representing Pascal's Triangle up to n rows.
 
     :param n: the number of rows to generate
     :type n: int
-    :return: a string representation of Pascal's Triangle up to n rows
-    :rtype: str
+    :return: a list of lists representing Pascal's Triangle up to n rows
+    :rtype: list[list[int]]
     """
     if n <= 0:
-        return ""
+        return []
 
+    # Initialize the triangle with the first row
     triangle = [[1]]
-    output = "1\n"
+
+    # Generate each subsequent row and add it to the triangle
     for i in range(1, n):
         row = [1]
-        output_row = "1"
         for j in range(1, i):
             val = triangle[i-1][j-1] + triangle[i-1][j]
             row.append(val)
-            output_row += " " + str(val)
         row.append(1)
         triangle.append(row)
-        output_row += " 1\n"
-        output += output_row
 
-    return output
+    # Format the triangle as a string with each row on a separate line
+    triangle_str = ""
+    for row in triangle:
+        row_str = " ".join([str(val) for val in row])
+        triangle_str += f"{row_str.center(n*2-1)}\n"
+
+    # Convert the formatted string back to a list of lists of integers
+    formatted_triangle = []
+    for row in triangle_str.strip().split("\n"):
+        formatted_row = [int(val) for val in row.split()]
+        formatted_triangle.append(formatted_row)
+
+    return formatted_triangle
