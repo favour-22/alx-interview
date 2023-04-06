ef773f6e0d91cba709a0d35b28acbd215c376995
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-04-07 00:03:38 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -21,18 +21,18 @@ def minOperations(n):
     if not isinstance(n, int):
         return 0
     ops_count = 0
-    counter = 0
+    clipboard = 0
     done = 1
     while done < n:
-        if counter == 0:
-            counter = done
-            done +=counter
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
             ops_count += 2
         elif n - done > 0 and (n - done) % done == 0:
-           counter = done
-            done += counter
+            clipboard = done
+            done += clipboard
             ops_count += 2
-        elif counter > 0:
-            done += counter
+        elif clipboard > 0:
+            done += clipboard
             ops_count += 1
     return ops_count
