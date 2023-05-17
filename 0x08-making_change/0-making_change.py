504c34a0e5ef8b3ff732382fe527e01398660784
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-05-17 13:30:20 +0100

    making chanfe

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def make_change(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest number of
+    coins needed to meet a given amount total.
+
+    :param coins: list of coin values
+    :param total: amount to reach
+    :return: fewest number of coins needed to reach total
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total+1)
+    dp[0] = 0
+
+    for c in coins:
+        for i in range(c, total+1):
+            dp[i] = min(dp[i], dp[i-c]+1)
+
+    return dp[total] if dp[total] != float('inf') else -1
